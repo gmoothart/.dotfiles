d8feeacda0a19d79cb9242ec41038addc9da048a
Gabe Moothart <gmoothart@gmail.com>
2010-08-11 15:45:31 -0700

    added powershell prompt modifications

--- Documents/WindowsPowerShell/profile.ps1
+++ Documents/WindowsPowerShell/profile.ps1
@@ -1,23 +1,41 @@
 
-function get-isadmin {
-  $windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-  $windowsPrincipal = new-object 'System.Security.Principal.WindowsPrincipal' $windowsIdentity
-
-  return $windowsPrincipal.IsInRole("Administrators")
+Import-Module Pscx
 
 
-}
+function prompt {
 
+    Write-Host("")
+    $status_string = ""
+    $forecolor = (Get-Host).UI.RawUI.ForegroundColor
 
-#
-# Show admin console as tark red
-#
-if ((get-isadmin) -and (-not $psISE)) {
-    $Host.UI.RawUI.BackgroundColor = "DarkRed"
-    clear
-}
+    # 
+    # mark administrator
+    #
+    if ($pscx:isadmin -and (-not $psISE)) {
+      $status_string += "ADMIN "
+    }
 
+    #
+    # Add git prompt
+    # http://stackoverflow.com/questions/1287718/how-can-i-display-my-current-git-branch-name-in-my-powershell-prompt/1287895#1287895
+    #
+    $symbolicref = git symbolic-ref HEAD
+    if($symbolicref -ne $NULL) {
+        $status_string += "GIT [" + $symbolicref.substring($symbolicref.LastIndexOf("/") +1) + "] "
 
+        # change prompt color if changes
+        $differences = (git diff-index --name-status HEAD)
+        if ($differences) {
+          $forecolor = "yellow"
+        }
+    }
+    else {
+        $status_string += "PS "
+    }
 
-# Add git prompt!
-#http://stackoverflow.com/questions/1287718/how-can-i-display-my-current-git-branch-name-in-my-powershell-prompt/1287895#1287895
+    #
+    # print out prompt
+    #
+    Write-Host ($status_string + $(get-location) + "`n>") -nonewline -foregroundcolor $forecolor
+    return " "
+ }
