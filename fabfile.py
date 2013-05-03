4fea7a156045b7bd532e3e776d5d56413c0ea44c
Gabe Moothart <gmoothart@gmail.com>
2013-05-03 09:35:09 -0700

    explicitly sudo when necessary, don't require sudo fab

--- fabfile.py
+++ fabfile.py
@@ -5,20 +5,20 @@ import os
 
 # setup that is not ubuntu version-dependent
 def _prepare_base():
-    local("apt-get install curl")
-    local("apt-get install zsh")
-    local("apt-get install build-essential")
+    local("sudo apt-get install curl")
+    local("sudo apt-get install zsh")
+    local("sudo apt-get install build-essential")
 
 
 
 # 10.04-specific setup
 def prepare_dev_lucid():
     _prepare_base()
-    local("apt-get install git-core")
+    local("sudo apt-get install git-core")
 
 
 # Setup for current ubuntu (may not work on older versions)
 def prepare_dev():
     _prepare_base()
-    local("apt-get install git")
+    local("sudo apt-get install git")
 
