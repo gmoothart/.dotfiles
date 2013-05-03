b73b482d17ffe1f7e276e32b5e00c76ad1e23ae2
Gabe Moothart <gmoothart@gmail.com>
2013-05-03 09:31:02 -0700

    branch on ubuntu version. Do this automatically someday

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,22 @@ from fabric.contrib.console import confirm
 import os
 
 
-def prepare_dev():
+# setup that is not ubuntu version-dependent
+def _prepare_base():
     local("apt-get install curl")
-    local("apt-get install git")
     local("apt-get install zsh")
     local("apt-get install build-essential")
+
+
+
+# 10.04-specific setup
+def prepare_dev_lucid():
+    _prepare_base()
+    local("apt-get install git-core")
+
+
+# Setup for current ubuntu (may not work on older versions)
+def prepare_dev():
+    _prepare_base()
+    local("apt-get install git")
+
