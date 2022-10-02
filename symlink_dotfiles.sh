be9b9f1a9adaddfc04ffb9e6c24d4c165643f142
Gabe Moothart <gmoothart@gmail.com>
2022-10-02 13:17:36 -0700

    docker updates; added bedrock

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 find ~/.dotfiles/ -maxdepth 1 -type f -name ".*" -exec ln -s -f {} $HOME \;
-ln -s -f ~/.dotfiles/.vim ~/.dotfiles/.emacs.d ~/.dotfiles/.config $HOME
+ln -s -f ~/.dotfiles/.vim ~/.dotfiles/.emacs.d $HOME
+ln -s -f ~/.dotfiles/docker-compose.yml $HOME
